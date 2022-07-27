8a7284140c231900d3d81b5d8a183d9f108a0a71
Roman Savka <roman.savka@miquido.com>
2022-07-27 12:29:45 +0200

    Update DolbyRendersFactory to recognize dovi render

--- android-exoplayer/src/main/java/com/brentvatne/exoplayer/DolbyRendersFactory.java
+++ android-exoplayer/src/main/java/com/brentvatne/exoplayer/DolbyRendersFactory.java
@@ -3,18 +3,13 @@ package com.brentvatne.exoplayer;
 import android.content.Context;
 import android.os.Handler;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-
 import com.google.android.exoplayer2.DefaultRenderersFactory;
 import com.google.android.exoplayer2.Renderer;
-import com.google.android.exoplayer2.audio.AudioProcessor;
 import com.google.android.exoplayer2.audio.AudioRendererEventListener;
 import com.google.android.exoplayer2.audio.AudioSink;
-import com.google.android.exoplayer2.drm.DrmSessionManager;
-import com.google.android.exoplayer2.drm.FrameworkMediaCrypto;
 import com.google.android.exoplayer2.mediacodec.MediaCodecSelector;
 import com.google.android.exoplayer2.util.Log;
+import com.google.android.exoplayer2.video.VideoRendererEventListener;
 
 import java.lang.reflect.Constructor;
 import java.util.ArrayList;
@@ -63,12 +58,68 @@ public class DolbyRendersFactory extends DefaultRenderersFactory {
             out.add(extensionRendererIndex, renderer);
             Log.i(TAG, "Loaded LibDaaAudioRenderer.");
         } catch (ClassNotFoundException e) {
-            Log.e(TAG, "Unable to load LibDaaAudioRenderer.", e);
             // Expected if the app was built without the extension.
+            Log.e(TAG, "Unable to load LibDaaAudioRenderer.", e);
         } catch (Exception e) {
             Log.e(TAG, "Unable to load LibDaaAudioRenderer.", e);
             // The extension is present, but instantiation failed.
             throw new RuntimeException("Error instantiating DAA extension", e);
+        }
+    }
+
+    @Override
+    protected void buildVideoRenderers(
+            Context context,
+            int extensionRendererMode,
+            MediaCodecSelector mediaCodecSelector,
+            boolean enableDecoderFallback,
+            Handler eventHandler,
+            VideoRendererEventListener eventListener,
+            long allowedVideoJoiningTimeMs,
+            ArrayList<Renderer> out) {
+        super.buildVideoRenderers(context, extensionRendererMode, mediaCodecSelector,
+                enableDecoderFallback, eventHandler, eventListener, allowedVideoJoiningTimeMs, out);
+
+        if (extensionRendererMode == EXTENSION_RENDERER_MODE_OFF) {
+            return;
+        }
+        int extensionRendererIndex = out.size();
+        if (extensionRendererMode == EXTENSION_RENDERER_MODE_PREFER) {
+            extensionRendererIndex--;
+        }
+
+        try {
+            // Full class names used for constructor args so the LINT rule triggers if any of them move.
+            // LINT.IfChange
+            Class<?> clazz = Class.forName("com.dolby.dovi.MediaCodecDoviRenderer");
+            Constructor<?> constructor =
+                    clazz.getConstructor(
+                            android.content.Context.class,
+                            com.google.android.exoplayer2.mediacodec.MediaCodecSelector.class,
+                            long.class,
+                            boolean.class,
+                            android.os.Handler.class,
+                            com.google.android.exoplayer2.video.VideoRendererEventListener.class,
+                            int.class);
+            // LINT.ThenChange(../../../../../../../proguard-rules.txt)
+            Renderer renderer =
+                    (Renderer)
+                            constructor.newInstance(
+                                    context,
+                                    MediaCodecSelector.DEFAULT,
+                                    allowedVideoJoiningTimeMs,
+                                    false,
+                                    eventHandler,
+                                    eventListener,
+                                    MAX_DROPPED_VIDEO_FRAME_COUNT_TO_NOTIFY);
+            out.add(extensionRendererIndex, renderer);
+            Log.i(TAG, "Loaded MediaCodecDoviRenderer.");
+        } catch (ClassNotFoundException e) {
+            // Expected if the app was built without the extension.
+            Log.e(TAG, "Unable to load MediaCodecDoviRenderer.", e);
+        } catch (Exception e) {
+            // The extension is present, but instantiation failed.
+            throw new RuntimeException("Error instantiating DOVI extension", e);
         }
     }
 }
